b34b6bf3371459307546d1d502e27bf05a2fb2e7
zhaolu138969 <13527571887@163.com>
2018-05-17 12:09:32 +0800

    “第二次提交”

--- alien_invasion.py
+++ alien_invasion.py
@@ -3,12 +3,13 @@ import pygame
 from settings import  Settings
 from ship import Ship
 import game_functions as gf
+from pygame.sprite import Group
+
 
 
 def run_game():
     #初始化游戏并创建屏幕对象
     pygame.init()
-    #screen = pygame.display.set_mode((1200,800))
     ai_settings = Settings()
     screen = pygame.display.set_mode((ai_settings.screen_width,ai_settings.screen_height))
     pygame.display.set_caption("Alien Invasion")
@@ -19,11 +20,15 @@ def run_game():
     #创建一艘飞船
     ship = Ship(ai_settings,screen)
 
+    #创建一个用于存储子弹的编组
+    bullets = Group()
+
     #开始游戏的主循环
     while True:
-        gf.check_events(ship)
+        gf.check_events(ai_settings, screen, ship, bullets)
         ship.update()
-        gf.update_screen(ai_settings,screen,ship)
+        bullets.update()
+        gf.update_screen(ai_settings, screen, ship, bullets)
 
 
 run_game()
--- game_functions.py
+++ game_functions.py
@@ -1,7 +1,8 @@
 import sys
 import pygame
+from bullet import Bullet
 
-def check_keydown_evens(event,ship):
+def check_keydown_events(event, ai_settings, screen, ship, bullets):
     """响应按键"""
     if event.key == pygame.K_RIGHT:
     # 向右移动飞船
@@ -9,30 +10,19 @@ def check_keydown_evens(event,ship):
     elif event.key == pygame.K_LEFT:
         ship.moving_left = True
 
+    #创建一个子弹，并将加入到编组bullets中
+    elif event.key == pygame.K_SPACE:
+        new_bullet = Bullet(ai_settings, screen, ship)
+        bullets.add(new_bullet)
+
 def check_keyup_events(event,ship):
     if event.key == pygame.K_RIGHT:
         ship.moving_right = False
     elif event.key == pygame.K_LEFT:
         ship.moving_left = False
 
-# def check_keydown_evens(event,ship):
-#     """响应按键"""
-#     if event.key == pygame.K_UP:
-#     # 向上移动飞船
-#         ship.moving_up = True
-#     elif event.key == pygame.K_DOWN:
-#         ship.moving_down = True
-#
-#
-# def check_keyup_events(event,ship):
-#     if event.key == pygame.K_UP:
-#         ship.moving_up= False
-#     elif event.key == pygame.K_DOWN:
-#         ship.moving_down = False
 
-
-
-def check_events(ship):
+def check_events(ai_settings, screen, ship, bullets):
     """响应按键和鼠标的事件"""
 
     for event in pygame.event.get():
@@ -40,14 +30,17 @@ def check_events(ship):
             sys.exit()
 
         elif event.type == pygame.KEYDOWN:
-            check_keydown_evens(event,ship)
+            check_keydown_events(event, ai_settings, screen, ship, bullets)
         elif event.type == pygame.KEYUP:
             check_keyup_events(event,ship)
 
 
-def update_screen(ai_settings,screen,ship):
+def update_screen(ai_settings,screen,ship, bullets):
     """"更新屏幕上的图像，并切到到新屏幕"""
     screen.fill(ai_settings.bg_color)
+    #在飞船和外星人后面重新绘制所有的子弹
+    for bullet in bullets.sprites():
+        bullet.draw_bullet()
     ship.blitme()
 
     # 让最近的绘制的屏幕可见
